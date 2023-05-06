606ff9f36b05850591814d2dda40b1aa8e38bba1
taleb <talebbahan@gmail.com>
2023-05-06 07:12:25 -1200

    fsa

---
+++ client/src/views/admin/newsletter/newsletter/Preview.jsx
@@ -0,0 +1,42 @@
+// import { Fragment, useState } from "react";
+// import {
+//   Button,
+//   Dialog,
+//   DialogHeader,
+//   DialogBody,
+//   DialogFooter,
+//   Textarea,
+// } from "@material-tailwind/react";
+// import Upload from "./Upload";
+
+
+// export default function AddArticle(props) {
+
+//   return (
+//     <Fragment>
+
+//       <Dialog open={props.open} handler={props.handleOpen} size='xxl'>
+
+//         <DialogHeader>{props.isAdd ? 'Create New Newsletter' : 'Edit Newsletter'}</DialogHeader>
+//         <DialogBody divider>
+          
+//         </DialogBody>
+
+//         <DialogFooter>
+//           <Button
+//             variant="text"
+//             color="red"
+//             onClick={props.handleOpen}
+//             class="mr-1"
+//           >
+//             <span>Cancel</span>
+//           </Button>
+//           <Button variant="text" color="green" type="submit">
+//             <span>Confirm</span>
+//           </Button>
+//         </DialogFooter>
+//       </Dialog>
+
+//     </Fragment >
+//   );
+// }
