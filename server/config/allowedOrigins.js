585cf9f9b33bef683360205258d066d116339c47
taleb <talebbahan@gmail.com>
2023-05-16 05:10:10 -1200

    fgdhd

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
  process.env.FRONTEND_URI,
-    'http://127.0.0.1:5500',
+    'https://eddydashboard.onrender.com',
     'http://localhost:3500',
     'http://localhost:3000',
     'http://localhost:3001',
