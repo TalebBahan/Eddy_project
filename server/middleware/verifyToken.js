856d9c9e76eb238a6988a4b334d6a722404e400b
a7med_sjd <21020@supnum.mr>
2023-05-23 10:54:20 +0000

    verify google token

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,42 @@
+// middelware to verify google token 
+const { OAuth2Client } = require('google-auth-library');
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URL);
+const tokens = require('../models/tokens');
+
+const refreshgoogleAccessToken = async (refreshToken) => {
+    const { data } = await axios({
+        url: 'https://oauth2.googleapis.com/token',
+        method: 'post',
+        params: {
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            client_secret: process.env.GOOGLE_CLIENT_SECRET,
+            refresh_token: refreshToken,
+            grant_type: 'refresh_token',
+        },
+    });
+    return data.access_token;
+};
+
+const verifyToken = async (req, res, next) => {
+    try {
+        const token = await Token.findOne({}); // Assuming you have only one token in your Token collection
+        const newAccessToken = await refreshgoogleAccessToken(token.googleRefreshToken); // Function to refresh the Google access token
+  
+        if (token.googleAccessToken !== newAccessToken) {
+          // Update the token in the database
+          token.googleAccessToken = newAccessToken;
+          await token.save();
+        }
+  
+        // Proceed to the next middleware
+        next();
+      } catch (error) {
+        // Handle any errors that occurred during token refreshing
+        res.status(500).json({ error: 'Failed to refresh access token' });
+      }
+    };
+
+
+
+module.exports = verifyToken;
