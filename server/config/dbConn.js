fb6a3b88aa284b1cc816c5d4b53af93fcd295815
talebbahan <talebbahan@gmail.com>
2024-10-07 16:57:29 +0000

    ff

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        mongoose.set('strictQuery', true);
+        // mongoose.set('strictQuery', true);
 
         console.log('Connecting to MongoDB...');
         console.log(process.env.DATABASE_URI2);
