d73a20f0fda4d971ac9d4dbf8747ef1a1538b5b8
talebbahan <talebbahan@gmail.com>
2024-10-07 16:46:56 +0000

    fix

--- server/config/dbConn.js
+++ server/config/dbConn.js
@@ -5,10 +5,10 @@ const connectDB = async () => {
         mongoose.set('strictQuery', true);
 
         console.log('Connecting to MongoDB...');
-        console.log(process.env.DATABASE_URI);
+        console.log(process.env.DATABASE_URI2);
 
 
-        await mongoose.connect(process.env.DATABASE_URI, {
+        await mongoose.connect(process.env.DATABASE_URI2, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         });
